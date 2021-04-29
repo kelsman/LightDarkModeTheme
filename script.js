aeaa4dc97fc10a6c248f91e75aa34629a695ff09
kelvin <osekelvin22@gmail.com>
2021-04-29 01:42:37 +0100

    code cleanup

--- script.js
+++ script.js
@@ -14,21 +14,14 @@ const imageMode = (color) => {
     image2.setAttribute("src", `./img/undraw_feeling_proud_${color}.svg`)
     image3.setAttribute("src", `./img/undraw_conceptual_idea_${color}.svg`)
 }
-// dark mode function
-const darkMode = () => {
-    nav.style.backgroundColor = "rgb(0 0 0 /50%)";
-    textBox.style.backgroundColor = "rgb(255 255 255/ 50%)";
-    toggleIcon.children[0].textContent = "Dark Mode"
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark')
-}
 
-const lightMode = () => {
-    toggleIcon.children[0].textContent = "Light Mode"
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light')
-    nav.style.backgroundColor = "rgb(255 255 255/ 50%)";
-    textBox.style.backgroundColor = "rgb(0 0 0 /50%)";
+function toggleLightMode(isDark) {
+    nav.style.backgroundColor = isDark ? "rgb(0 0 0 /50%)" : "rgb(255 255 255/ 50%)";
+    textBox.style.backgroundColor = isDark ? "rgb(255 255 255/ 50%)" : "rgb(0 0 0 /50%)"
+    toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :
+        toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode('dark') : imageMode('light')
 }
 
 //  switch theme function
@@ -37,11 +30,11 @@ function switchTheme(event) {
     if (checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark')
-        darkMode();
+        toggleLightMode(true)
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light')
-        lightMode();
+        toggleLightMode(false)
     }
 }
 // Event Listerner
@@ -54,9 +47,9 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
-        darkMode()
+        toggleLightMode(true)
     } else if (currentTheme === "light") {
         toggleSwitch.checked = false;
-        darkMode()
+        toggleLightMode(false)
     }
 }
